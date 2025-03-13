1970cf6679b06eabf2079584b23e7c09bbd82b22
Aaron <aaronmu7@gmail.com>
2025-03-13 21:32:07 +0800

    添加 cities.js

---
+++ cities.js
@@ -0,0 +1,183 @@
+// 中国主要城市数据
+const cities = [
+    {
+        name: "北京",
+        lat: 39.9042,
+        lng: 116.4074,
+        id: "beijing"
+    },
+    {
+        name: "上海",
+        lat: 31.2304,
+        lng: 121.4737,
+        id: "shanghai"
+    },
+    {
+        name: "广州",
+        lat: 23.1291,
+        lng: 113.2644,
+        id: "guangzhou"
+    },
+    {
+        name: "深圳",
+        lat: 22.5431,
+        lng: 114.0579,
+        id: "shenzhen"
+    },
+    {
+        name: "成都",
+        lat: 30.5728,
+        lng: 104.0668,
+        id: "chengdu"
+    },
+    {
+        name: "杭州",
+        lat: 30.2741,
+        lng: 120.1551,
+        id: "hangzhou"
+    },
+    {
+        name: "武汉",
+        lat: 30.5928,
+        lng: 114.3055,
+        id: "wuhan"
+    },
+    {
+        name: "西安",
+        lat: 34.3416,
+        lng: 108.9398,
+        id: "xian"
+    },
+    {
+        name: "重庆",
+        lat: 29.4316,
+        lng: 106.9123,
+        id: "chongqing"
+    },
+    {
+        name: "南京",
+        lat: 32.0603,
+        lng: 118.7969,
+        id: "nanjing"
+    },
+    {
+        name: "天津",
+        lat: 39.3434,
+        lng: 117.3616,
+        id: "tianjin"
+    },
+    {
+        name: "苏州",
+        lat: 31.2990,
+        lng: 120.5853,
+        id: "suzhou"
+    },
+    {
+        name: "哈尔滨",
+        lat: 45.8038,
+        lng: 126.5340,
+        id: "harbin"
+    },
+    {
+        name: "长春",
+        lat: 43.8171,
+        lng: 125.3235,
+        id: "changchun"
+    },
+    {
+        name: "沈阳",
+        lat: 41.8057,
+        lng: 123.4315,
+        id: "shenyang"
+    },
+    {
+        name: "长沙",
+        lat: 28.2282,
+        lng: 112.9388,
+        id: "changsha"
+    },
+    {
+        name: "郑州",
+        lat: 34.7466,
+        lng: 113.6253,
+        id: "zhengzhou"
+    },
+    {
+        name: "济南",
+        lat: 36.6512,
+        lng: 117.1201,
+        id: "jinan"
+    },
+    {
+        name: "青岛",
+        lat: 36.0671,
+        lng: 120.3826,
+        id: "qingdao"
+    },
+    {
+        name: "大连",
+        lat: 38.9140,
+        lng: 121.6147,
+        id: "dalian"
+    },
+    {
+        name: "厦门",
+        lat: 24.4798,
+        lng: 118.0894,
+        id: "xiamen"
+    },
+    {
+        name: "福州",
+        lat: 26.0745,
+        lng: 119.2965,
+        id: "fuzhou"
+    },
+    {
+        name: "昆明",
+        lat: 25.0389,
+        lng: 102.7183,
+        id: "kunming"
+    },
+    {
+        name: "南宁",
+        lat: 22.8170,
+        lng: 108.3665,
+        id: "nanning"
+    },
+    {
+        name: "拉萨",
+        lat: 29.6500,
+        lng: 91.1000,
+        id: "lhasa"
+    },
+    {
+        name: "乌鲁木齐",
+        lat: 43.8256,
+        lng: 87.6168,
+        id: "urumqi"
+    },
+    {
+        name: "银川",
+        lat: 38.4872,
+        lng: 106.2309,
+        id: "yinchuan"
+    },
+    {
+        name: "呼和浩特",
+        lat: 40.8427,
+        lng: 111.7498,
+        id: "hohhot"
+    },
+    {
+        name: "香港",
+        lat: 22.3193,
+        lng: 114.1694,
+        id: "hongkong"
+    },
+    {
+        name: "澳门",
+        lat: 22.1987,
+        lng: 113.5439,
+        id: "macau"
+    }
+];
